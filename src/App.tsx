f3905718f28ec994cad536f37b7cc725774d910d
ZzYyfan <A80312@gree.com.cn>
2024-07-21 20:15:40 +0800

    feat: react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import { Button } from 'antd'
 
-const App = () => (
-  <div className='App'>
-    <Button type='primary'>Button</Button>
-  </div>
-)
+const Home = () => {
+  return <h2>Home</h2>
+}
+
+const About = () => {
+  return <h2>About</h2>
+}
+
+const App = () => {
+  return (
+    <Router>
+      <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link>
+      <Button type='primary'>Button</Button>
+      <Routes>
+        <Route path='/about' element={<About />} />
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
+  )
+}
 
 export default App
