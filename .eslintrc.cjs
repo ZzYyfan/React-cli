ed855b583783c00a261babca868e89bd55c0c776
ZzYyfan <A80312@gree.com.cn>
2024-07-21 18:42:20 +0800

    feat:修改eslint prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,52 @@
 module.exports = {
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    node: true,
+    es2021: true
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    jsxPragma: 'React',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ['@typescript-eslint', 'react-hooks', 'prettier'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
+    'prettier/prettier': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'no-restricted-syntax': ['error', 'LabeledStatement', 'WithStatement'],
+    'no-var': 'error',
+    'no-empty': ['error', { allowEmptyCatch: true }],
+    'no-void': 'error',
+    'prefer-const': [
       'warn',
-      { allowConstantExport: true },
+      { destructuring: 'all', ignoreReadBeforeAssign: true }
     ],
-  },
+    'prefer-template': 'error',
+    'object-shorthand': [
+      'error',
+      'always',
+      { ignoreConstructors: false, avoidQuotes: true }
+    ],
+    'block-scoped-var': 'error',
+    'no-constant-condition': ['error', { checkLoops: false }],
+    'no-redeclare': 'off',
+    semi: ['error', 'never']
+  }
 }
